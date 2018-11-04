d2bb7d6602ad4a913fa05336146f50b9f7bb46fa
Rooby Chen <rooby.chen@gmail.com>
2018-11-04 19:11:07 +0800

    test

--- app.js
+++ app.js
@@ -1,12 +1,14 @@
 const http = require('http');
+const fs = require('fs');
 
-const hostname = '127.0.0.1';
-const port = 3000;
+const hostname = 'localhost';
+const port = 8000;
+
 
 const server = http.createServer((req, res) => {
     res.statusCode = 200;
-    res.setHeader('Content-Type', 'text/plain');
-    res.end('Hello World\n');
+    res.setHeader('Content-Type', 'text/html');
+    res.end('hello world\n');
 });
 
 server.listen(port, hostname, () => {
--- myRallyTool.js
+++ myRallyTool.js
@@ -3,20 +3,31 @@ angular.module('myRallyTool', [])
 	.controller('myRallyController', ['$scope','$http', function($scope, $http) {
 		let us = $scope;
 		us.userStory = {
-			Name: '',
-			Owner: '',
-			ScheduleState: 'Backlog',
-			c_CRM: '',
-			c_Siebel: '',
-			DisplayColor: '',
-			tag: '',
-			towner: ''
+			uStory: {
+				Name: '',
+				Owner: {},
+				ScheduleState: 'Backlog',
+				c_CRM: '',
+				c_Siebel: '',
+				DisplayColor: '',
+				Iteration: {},
+				tag: {}
+			},
+			other:{
+				towner: {},
+				pureName: '',
+				utype: '',
+				app: ''
+			}
 		};
 
 		us.user = [];
 		us.proty = [];
 		us.tags = [];
+		us.itert = [];
+		us.utype = ['Backend', 'Frontend', 'Both end'];
 		us.selected = {};
+		us.res = '';
 
 		$http.get('./owner.json').then((response) => {
 			us.user = response.data;
@@ -31,8 +42,28 @@ angular.module('myRallyTool', [])
 			us.tags = response.data;
 		});
 
-		us.create = () => {
-			alert(us.userStory.c_CRM + '/' + us.userStory.Owner);
+		$http.get('./itert.json').then((response) => {
+			us.itert = response.data;
+		});
+
+		this.create = () => {
+			us.userStory.uStory.Name = us.userStory.other.pureName + '-' + us.userStory.uStory.c_CRM;
+			alert(JSON.stringify(us.userStory.uStory.Name));
+			// $http.post('http://127.0.0.1:3000/userstory/create',JSON.stringify(us.userStory),{
+   //              headers : {
+   //                  'Content-Type': 'application/x-www-form-urlencoded;charset=utf-8;'
+   //              }
+   //          }).then((response) => {
+				
+			// });
+			$http({
+				method: 'POST',
+				url: 'http://127.0.0.1:3000/userstory/create',
+				data: JSON.stringify(us.userStory),
+				headers:{'Content-Type': 'application/x-www-form-urlencoded'},
+			}).then((response) =>{
+
+			});
 		};
 
 	}]);
